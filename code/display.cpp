5c35e38d9c5c89ff02e64742ec0083a4a8b274d9
Xose Pérez <xose.perez@gmail.com>
2023-05-01 12:47:45 +0200

    Refactor

--- code/display.cpp
+++ code/display.cpp
@@ -8,7 +8,8 @@
 #include "bluetooth.h"
 #include "cube.h"
 #include "utils.h"
-#include "bmp_cube.h"
+
+#include "assets/bmp_cube.h"
 
 #define DISPLAY_ALIGN_LEFT    0
 #define DISPLAY_ALIGN_CENTER  1
@@ -17,7 +18,7 @@
 #define DISPLAY_ALIGN_MIDDLE  4
 #define DISPLAY_ALIGN_BOTTOM  8
 
-Adafruit_ST7789 display_tft = Adafruit_ST7789(DISPLAY_CS_GPIO, DISPLAY_DC_GPIO, DISPLAY_RST_GPIO);
+Adafruit_ST7789 _display_screen = Adafruit_ST7789(DISPLAY_CS_GPIO, DISPLAY_DC_GPIO, DISPLAY_RST_GPIO);
 
 // ----------------------------------------------------------------------------
 // Private
@@ -28,21 +29,21 @@ static void display_draw_bmp(const GUI_BITMAP *bmp , uint8_t x, uint8_t y) {
     uint16_t color = bmp->date[0];
     uint32_t count = 0;
     uint64_t bufSize = bmp->xSize * bmp->ySize;
-    display_tft.startWrite();
-    display_tft.setAddrWindow(x, y, bmp->xSize, bmp->ySize);
+    _display_screen.startWrite();
+    _display_screen.setAddrWindow(x, y, bmp->xSize, bmp->ySize);
 
     for ( uint64_t i = 0 ; i < bufSize ; i++ ) {
         if(color == bmp->date[i]) {
             count++;
         } else {
-            display_tft.writeColor(color, count); 
+            _display_screen.writeColor(color, count); 
             count = 1;
             color = bmp->date[i];
         }
     }
 
-    display_tft.writeColor(color, count); 
-    display_tft.endWrite();
+    _display_screen.writeColor(color, count); 
+    _display_screen.endWrite();
 
 }
 
@@ -53,12 +54,12 @@ void display_update_cube() {
 
     // URFDLB
     uint16_t colors[6] = { 
-      display_tft.color565(255, 255, 255), 
-      display_tft.color565(255,   0,   0), 
-      display_tft.color565(  0, 144,   0), 
-      display_tft.color565(255, 255,   0), 
-      display_tft.color565(255, 144,  48), 
-      display_tft.color565(  0,   0, 255)
+      _display_screen.color565(255, 255, 255), 
+      _display_screen.color565(255,   0,   0), 
+      _display_screen.color565(  0, 144,   0), 
+      _display_screen.color565(255, 255,   0), 
+      _display_screen.color565(255, 144,  48), 
+      _display_screen.color565(  0,   0, 255)
     };
     
     // translate cubelets to colors
@@ -85,42 +86,42 @@ void display_update_cube() {
     x = offset_x + 3 * block;
     y = offset_y + 3 * block;
     for (uint8_t i=0; i<3; i++) for (uint8_t j=0; j<3; j++) {
-      display_tft.fillRect(x + j * block, y + i * block, size, size, colors[cc[index++]]);
+      _display_screen.fillRect(x + j * block, y + i * block, size, size, colors[cc[index++]]);
     }
 
     // R
     x = offset_x + 6 * block;
     y = offset_y + 3 * block;
     for (uint8_t i=0; i<3; i++) for (uint8_t j=0; j<3; j++) {
-      display_tft.fillRect(x + i * block, y + (2-j) * block, size, size, colors[cc[index++]]);
+      _display_screen.fillRect(x + i * block, y + (2-j) * block, size, size, colors[cc[index++]]);
     }
 
     // F
     x = offset_x + 3 * block;
     y = offset_y + 6 * block;
     for (uint8_t i=0; i<3; i++) for (uint8_t j=0; j<3; j++) {
-      display_tft.fillRect(x + j * block, y + i * block, size, size, colors[cc[index++]]);
+      _display_screen.fillRect(x + j * block, y + i * block, size, size, colors[cc[index++]]);
     }
 
     // D
     x = offset_x + 9 * block;
     y = offset_y + 3 * block;
     for (uint8_t i=0; i<3; i++) for (uint8_t j=0; j<3; j++) {
-      display_tft.fillRect(x + (2-j) * block, y + (2-i) * block, size, size, colors[cc[index++]]);
+      _display_screen.fillRect(x + (2-j) * block, y + (2-i) * block, size, size, colors[cc[index++]]);
     }
 
     // L
     x = offset_x + 0 * block;
     y = offset_y + 3 * block;
     for (uint8_t i=0; i<3; i++) for (uint8_t j=0; j<3; j++) {
-      display_tft.fillRect(x + (2-i) * block, y + j * block, size, size, colors[cc[index++]]);
+      _display_screen.fillRect(x + (2-i) * block, y + j * block, size, size, colors[cc[index++]]);
     }
 
     // B
     x = offset_x + 3 * block;
     y = offset_y + 0 * block;
     for (uint8_t i=0; i<3; i++) for (uint8_t j=0; j<3; j++) {
-      display_tft.fillRect(x + (2-j) * block, y + (2-i) * block, size, size, colors[cc[index++]]);
+      _display_screen.fillRect(x + (2-j) * block, y + (2-i) * block, size, size, colors[cc[index++]]);
     }
 
 }
@@ -130,7 +131,7 @@ void display_text(char * text, uint16_t x, uint16_t y, uint8_t align) {
     // Get text dimensions
     int16_t a, b;
     uint16_t w, h;
-    display_tft.getTextBounds(text, 0, 0, &a, &b, &w, &h);
+    _display_screen.getTextBounds(text, 0, 0, &a, &b, &w, &h);
 
     // Calculate position
     if (align & DISPLAY_ALIGN_CENTER) x = x - (w / 2);
@@ -139,8 +140,8 @@ void display_text(char * text, uint16_t x, uint16_t y, uint8_t align) {
     if (align & DISPLAY_ALIGN_BOTTOM) y = y - h;
 
     // Display
-    display_tft.setCursor(x, y);
-    display_tft.println(text);    
+    _display_screen.setCursor(x, y);
+    _display_screen.println(text);    
 
 }
 
@@ -149,7 +150,7 @@ void display_text(char * text, uint16_t x, uint16_t y, uint8_t align) {
 // ----------------------------------------------------------------------------
 
 void display_clear() {
-    display_tft.fillScreen(ST77XX_BLACK);
+    _display_screen.fillScreen(ST77XX_BLACK);
 }
 
 void display_battery() {
@@ -160,22 +161,22 @@ void display_battery() {
     battery = utils_get_battery();
     sprintf(buffer, "BAT %02d%%", battery);
     if (battery > 25) {
-        display_tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+        _display_screen.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
     } else {
-        display_tft.setTextColor(ST77XX_RED, ST77XX_BLACK);
+        _display_screen.setTextColor(ST77XX_RED, ST77XX_BLACK);
     }
-    display_tft.setTextSize(1);
+    _display_screen.setTextSize(1);
     display_text(buffer, 310, 10, DISPLAY_ALIGN_RIGHT);
 
     battery = cube_get_battery();
     if (battery != 0xFF) {
         sprintf(buffer, "CUBE %02d%%", battery);
         if (battery > 25) {
-            display_tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+            _display_screen.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
         } else {
-            display_tft.setTextColor(ST77XX_RED, ST77XX_BLACK);
+            _display_screen.setTextColor(ST77XX_RED, ST77XX_BLACK);
         }
-        display_tft.setTextSize(1);
+        _display_screen.setTextSize(1);
         display_text(buffer, 310, 20, DISPLAY_ALIGN_RIGHT);
     }
 
@@ -185,18 +186,18 @@ void display_show_intro() {
     
     display_clear();
     display_draw_bmp(&bmp_cube_info, 20, 20);
-    display_tft.setTextSize(0);
-    display_tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+    _display_screen.setTextSize(0);
+    _display_screen.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
     display_text((char *) APP_NAME, 310, 210, DISPLAY_ALIGN_RIGHT | DISPLAY_ALIGN_BOTTOM);
     display_text((char *) APP_VERSION, 310, 220, DISPLAY_ALIGN_RIGHT | DISPLAY_ALIGN_BOTTOM);
-    display_tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
+    _display_screen.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
     display_text((char *) "Connect cube...", 310, 230, DISPLAY_ALIGN_RIGHT | DISPLAY_ALIGN_BOTTOM);
     display_battery();
 
 }
 
 void display_hide_timer() {
-    display_tft.fillRect(160, 180, 150, 40, ST77XX_BLACK);
+    _display_screen.fillRect(160, 180, 150, 40, ST77XX_BLACK);
 }
 
 void display_show_timer() {
@@ -215,8 +216,8 @@ void display_show_timer() {
     sprintf(buffer, "%02d:%02d.%03d", min, sec, ms);
 
 
-    display_tft.setTextColor((3 == status) ? ST77XX_WHITE : ST77XX_GREEN, ST77XX_BLACK);
-    display_tft.setTextSize(2);
+    _display_screen.setTextColor((3 == status) ? ST77XX_WHITE : ST77XX_GREEN, ST77XX_BLACK);
+    _display_screen.setTextSize(2);
     display_text(buffer, 226, 180, DISPLAY_ALIGN_CENTER);
 
     if (3 == status) {
@@ -226,14 +227,14 @@ void display_show_timer() {
         sprintf(buffer, "TURNS %d | TPS %2.1f", turns, tps);
     }
     
-    display_tft.setTextSize(1);
+    _display_screen.setTextSize(1);
     display_text(buffer, 226, 200, DISPLAY_ALIGN_CENTER);
 
 }
 
 void display_show_ready() {
-    display_tft.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
-    display_tft.setTextSize(2);
+    _display_screen.setTextColor(ST77XX_GREEN, ST77XX_BLACK);
+    _display_screen.setTextSize(2);
     display_text((char *) "READY...  ", 175, 180, 0);
 }
 
@@ -254,8 +255,8 @@ void display_setup(void) {
 
     display_on();
 
-    display_tft.init(240, 320);
-    display_tft.setRotation(3);
+    _display_screen.init(240, 320);
+    _display_screen.setRotation(3);
 
     #if DEBUG > 0
         Serial.printf("[TFT] Initialized\n");
